688e5bb28bc10310005cbfd2b142552b64eabd8f
Cagatay Sert <cagatay.sert5@gmail.com>
2025-05-11 16:58:10 +0300

    remove unused codes

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -23,28 +23,3 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     };
   }
 }
-
-// import { Injectable, UnauthorizedException } from '@nestjs/common';
-// import { PassportStrategy } from '@nestjs/passport';
-// import { ExtractJwt, Strategy } from 'passport-jwt';
-// import { UsersService } from '../users/users.service';
-// import { JwtPayload } from './interfaces/jwt-payload.interface';
-
-// @Injectable()
-// export class JwtStrategy extends PassportStrategy(Strategy) {
-//   constructor(private readonly usersService: UsersService) {
-//     super({
-//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//       ignoreExpiration: false,
-//       secretOrKey: process.env.JWT_SECRET_KEY,
-//     });
-//   }
-
-//   async validate(payload: JwtPayload): Promise<any> {
-//     const user = await this.usersService.findById(payload.sub);
-//     if (!user) {
-//       throw new UnauthorizedException();
-//     }
-//     return user;
-//   }
-// }
